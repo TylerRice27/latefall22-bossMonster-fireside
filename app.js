f721d9718edec6fb0113995189d66414cb0a3d32
TylerRice27 <Tylerrayrice2@gmail.com>
2022-11-03 17:55:02 -0600

    Code for class

--- app.js
+++ app.js
@@ -13,6 +13,39 @@ const boss = {
     level: 1
 }
 
+const companions = [
+    {
+        name: 'Wolverine',
+        type: 'dmg',
+        value: 5,
+        health: 150,
+        cost: 100,
+        power: 0,
+        img: 'https://media2.giphy.com/media/HTy0GcFDvOVi0/giphy.gif'
+    },
+    {
+        name: 'Iron-man',
+        type: 'dmg',
+        value: 15,
+        health: 100,
+        cost: 200,
+        power: 0,
+        img: "https://media1.giphy.com/media/7EiixpldfkXni/giphy.gif?cid=ecf05e470ymdtyll84dy0v9d66xiq0et3sqlzux9sf9ifv9c&rid=giphy.gif&ct=s"
+
+    },
+    {
+        name: 'Deadpool',
+        type: 'heal',
+        value: 10,
+        health: 100,
+        cost: 500,
+        power: 0,
+        img: "https://media3.giphy.com/media/TAbghspb8GP4I/200w.webp?cid=ecf05e47iusv43aubq8g15qe54v33cvtt327gqjf7nbaaouo&rid=200w.webp&ct=s"
+
+    }
+
+]
+
 
 function update() {
     // @ts-ignore
@@ -38,8 +71,8 @@ function attackBoss() {
         bossLevelUp()
     }
 
-    // let audio = new Audio('imgs/Spider-Man web shoot sound effect.mp3')
-    // audio.play()
+    let audio = new Audio('imgs/Spider-Man web shoot sound effect.mp3')
+    audio.play()
     update()
 
 }
@@ -65,4 +98,83 @@ function bossAttack() {
 }
 
 setInterval(bossAttack, 2000)
+
+
+function buy(name) {
+    let companion = companions.find(f => f.name == name)
+    console.log(companion);
+    if (!companion) {
+        return
+    }
+    if (hero.credit < companion.cost) {
+        window.alert("Go farm some more credits")
+        return
+    }
+
+    hero.credit -= companion.cost
+
+
+    companion.power++
+    console.log(companion.power);
+
+    drawCompanions(companion)
+    companionActions()
+    update()
+
+
+
+
+}
+
+function drawCompanions(companion) {
+    let template = `
+    <div class="d-flex flex-column align-items-center"
+    <div class="bg-dark rounded p-1">
+                    <h3 class="text-light  m-0 pt-2  text-center text-shadow">Level:${companion.power}</h3>
+                </div>
+                <img class="hero-img"
+                    src="${companion.img}"
+                    alt="">
+            </div>
+    `
+    document.getElementById(`${companion.name}`).innerHTML = template
+
+}
+
+function damageBoss(damage) {
+    boss.health -= damage
+    if (boss.health < 0) {
+        bossLevelUp()
+    }
+    update()
+}
+
+function healHero(val) {
+    hero.health += val
+    if (hero.health >= 100) {
+        hero.health = 100
+    }
+    update()
+
+}
+
+function companionActions() {
+    companions.forEach(c => {
+        console.log(c);
+        if (c.health <= 0) {
+            c.health = 0
+            return
+        } if (c.type == "dmg") {
+            let damage = c.power * c.value
+            damageBoss(damage)
+        } else {
+            let heal = c.power * c.value
+            healHero(heal)
+        }
+
+    })
+
+}
+
+setInterval(companionActions, 3000)
 
